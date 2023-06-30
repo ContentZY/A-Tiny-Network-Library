6d434ac16259d5f3513513954a0a8cecf9a3d54d
xidian_com <zhiyu_liang@163.com>
2023-06-30 17:15:07 +0800

    New web

---
+++ cmake-build-debug-/CMakeFiles/Myweb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Myweb.dir/main.cpp.o"
+  "CMakeFiles/Myweb.dir/main.cpp.o.d"
+  "Myweb"
+  "Myweb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Myweb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug-/CMakeFiles/tiny_network.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lzy/A-Tiny-Net/src/base/CurrentThread.cc" "CMakeFiles/tiny_network.dir/src/base/CurrentThread.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/base/CurrentThread.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/base/Thread.cc" "CMakeFiles/tiny_network.dir/src/base/Thread.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/base/Thread.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/base/ThreadPool.cc" "CMakeFiles/tiny_network.dir/src/base/ThreadPool.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/base/ThreadPool.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/base/Timestamp.cc" "CMakeFiles/tiny_network.dir/src/base/Timestamp.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/base/Timestamp.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/http/HttpContext.cc" "CMakeFiles/tiny_network.dir/src/http/HttpContext.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/http/HttpContext.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/http/HttpResponse.cc" "CMakeFiles/tiny_network.dir/src/http/HttpResponse.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/http/HttpResponse.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/http/HttpServer.cc" "CMakeFiles/tiny_network.dir/src/http/HttpServer.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/http/HttpServer.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/http/main.cc" "CMakeFiles/tiny_network.dir/src/http/main.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/http/main.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/logger/AsyncLogging.cc" "CMakeFiles/tiny_network.dir/src/logger/AsyncLogging.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/logger/AsyncLogging.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/logger/FileUtil.cc" "CMakeFiles/tiny_network.dir/src/logger/FileUtil.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/logger/FileUtil.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/logger/LogFile.cc" "CMakeFiles/tiny_network.dir/src/logger/LogFile.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/logger/LogFile.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/logger/LogStream.cc" "CMakeFiles/tiny_network.dir/src/logger/LogStream.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/logger/LogStream.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/logger/Logging.cc" "CMakeFiles/tiny_network.dir/src/logger/Logging.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/logger/Logging.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/memory/MemoryPool.cc" "CMakeFiles/tiny_network.dir/src/memory/MemoryPool.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/memory/MemoryPool.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/mysql/ConnectionPool.cc" "CMakeFiles/tiny_network.dir/src/mysql/ConnectionPool.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/mysql/ConnectionPool.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/mysql/MysqlConn.cc" "CMakeFiles/tiny_network.dir/src/mysql/MysqlConn.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/mysql/MysqlConn.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/net/Acceptor.cc" "CMakeFiles/tiny_network.dir/src/net/Acceptor.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/net/Acceptor.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/net/Buffer.cc" "CMakeFiles/tiny_network.dir/src/net/Buffer.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/net/Buffer.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/net/Channel.cc" "CMakeFiles/tiny_network.dir/src/net/Channel.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/net/Channel.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/net/EventLoop.cc" "CMakeFiles/tiny_network.dir/src/net/EventLoop.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/net/EventLoop.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/net/EventLoopThread.cc" "CMakeFiles/tiny_network.dir/src/net/EventLoopThread.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/net/EventLoopThread.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/net/EventLoopThreadPool.cc" "CMakeFiles/tiny_network.dir/src/net/EventLoopThreadPool.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/net/EventLoopThreadPool.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/net/InetAddress.cc" "CMakeFiles/tiny_network.dir/src/net/InetAddress.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/net/InetAddress.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/net/Socket.cc" "CMakeFiles/tiny_network.dir/src/net/Socket.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/net/Socket.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/net/TcpConnection.cc" "CMakeFiles/tiny_network.dir/src/net/TcpConnection.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/net/TcpConnection.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/net/TcpServer.cc" "CMakeFiles/tiny_network.dir/src/net/TcpServer.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/net/TcpServer.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/net/poller/DefaultPoller.cc" "CMakeFiles/tiny_network.dir/src/net/poller/DefaultPoller.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/net/poller/DefaultPoller.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/net/poller/EPollPoller.cc" "CMakeFiles/tiny_network.dir/src/net/poller/EPollPoller.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/net/poller/EPollPoller.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/net/poller/Poller.cc" "CMakeFiles/tiny_network.dir/src/net/poller/Poller.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/net/poller/Poller.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/timer/Timer.cc" "CMakeFiles/tiny_network.dir/src/timer/Timer.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/timer/Timer.cc.o.d"
+  "/home/lzy/A-Tiny-Net/src/timer/TimerQueue.cc" "CMakeFiles/tiny_network.dir/src/timer/TimerQueue.cc.o" "gcc" "CMakeFiles/tiny_network.dir/src/timer/TimerQueue.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
